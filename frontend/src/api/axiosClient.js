2b0b77729f3d307ad20d84fed2516a91ce00986a
Andrés David Caro Mora <ddaviddcaro@gmail.com>
2025-11-30 15:35:58 -0500

    Config: Apuntando frontend a la API de Render

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Creamos una instancia configurada
 const axiosClient = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api/', // La URL de tu Django
+    baseURL: 'https://api-enersinc-andres-caro.onrender.com/api/',
     headers: {
         'Content-Type': 'application/json',
     },
